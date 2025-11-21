67dcc7c654c768d06584f507a7b699067c6bda68
Yu-Wen Chen <r05223131@ntu.edu.tw>
2025-11-21 00:34:19 -0700

    ssfr atm corr update

--- lrt_sim/ssfr_atm_corr.py
+++ lrt_sim/ssfr_atm_corr.py
@@ -29,13 +29,13 @@ import os
 import sys
 import platform
 
-if platform.system() == 'Linux':
-    # Define the path to your module directory
-    # Use os.path.abspath and os.path.join for platform independence
-    module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'util'))
+# if platform.system() == 'Linux':
+#     # Define the path to your module directory
+#     # Use os.path.abspath and os.path.join for platform independence
+#     module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'util'))
 
-    # Add the directory to the Python search path
-    sys.path.insert(0, module_path)
+#     # Add the directory to the Python search path
+#     sys.path.insert(0, module_path)
 
 import glob
 import copy
