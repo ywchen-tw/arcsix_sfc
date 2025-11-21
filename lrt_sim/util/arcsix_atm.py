c23b42d555ff7e8f95177007355d26181dbd104b
Yu-Wen Chen <r05223131@ntu.edu.tw>
2025-11-21 01:10:05 -0700

    ssfr atm corr update

--- lrt_sim/util/arcsix_atm.py
+++ lrt_sim/util/arcsix_atm.py
@@ -35,6 +35,122 @@ from collections import defaultdict
 
 
 import er3t
+from er3t.util.modis import get_sinusoidal_grid_tag, get_filename_tag, download_modis_rgb
+from er3t.util.daac import download_laads_https
+from er3t.dev.calipso import download_calipso_vfm_http
+
+
+class modis_download:
+    """
+    This class is used to download satellite data from MODIS and OCO-2
+    """
+    def __init__(self,
+                 date=None,
+                 satID='aqua',
+                 extent=None,
+                 extent_analysis=None,
+                 fname=None,
+                 fdir_out='data',
+                 fdir_pre_data='data',
+                 filename_tag=None,
+                 overwrite=False,
+                 quiet=False,
+                 verbose=False):
+        """
+        Initialize the SatelliteDownload class.
+        """
+        self.date     = date
+        self.satID    = satID
+        self.extent   = extent
+        self.extent_analysis = extent_analysis
+        self.fdir_out = fdir_out
+        self.fdir_pre_data = fdir_pre_data
+        self.filename_tag = filename_tag
+        self.quiet    = quiet
+        self.verbose  = verbose
+
+        if (fname is not None) and os.path.exists(fname) and (not overwrite):
+            self.load(fname)
+        elif ((date is not None) and (extent is not None) and (fname is not None) and os.path.exists(fname) and overwrite) or \
+             ((date is not None) and (extent is not None) and (fname is not None) and not os.path.exists(fname)):
+            self.run()
+            self.dump(fname)
+
+        elif date is not None and extent is not None and fname is None:
+            self.run()
+        else:
+            raise FileNotFoundError('Error   [satellite_download]: Please check if \'%s\' exists or provide \'date\' and \'extent\' to proceed.' % fname)
+
+    def load(self, fname):
+        """
+        Load the satellite data from the pickle file.
+        """
+        with open(fname, 'rb') as f:
+            obj = pickle.load(f)
+            if hasattr(obj, 'fnames') and hasattr(obj, 'extent') and hasattr(obj, 'fdir_out') and hasattr(obj, 'date'):
+                if self.verbose:
+                    print('Message [satellite_download]: Loading %s ...' % fname)
+                self.date     = obj.date
+                self.extent   = obj.extent
+                self.fnames   = obj.fnames
+                self.fdir_out = obj.fdir_out
+            else:
+                sys.exit('Error   [satellite_download]: File \'%s\' is not the correct pickle file to load.' % fname)
+
+    def run(self, run=True):
+        """
+        Run the satellite download process.
+        """
+        lon = np.array(self.extent[:2])
+        lat = np.array(self.extent[2:])
+        if self.satID == 'aqua':
+            sat_tag = '61/MYD'
+        elif self.satID == 'terra':
+            sat_tag = '61/MOD'
+
+        if self.filename_tag is None:
+            raise ValueError('Error   [satellite_download]: Please provide filename_tag for MODIS data download.')
+
+
+        # modis_parent_fdir = '/'.join([self.fdir_out, 'modis'])
+        modis_parent_fdir = self.fdir_out
+        if not os.path.exists(modis_parent_fdir):
+            os.makedirs(modis_parent_fdir)
+        modis_fdir = f"{modis_parent_fdir}/{self.date.strftime('%Y%m%d')}"
+        if not os.path.exists(modis_fdir):
+            os.makedirs(modis_fdir)
+        
+        self.fnames = {}
+        # MODIS Level 2 Cloud Product
+        # self.fnames['mod_l2'] = download_laads_https(self.date, f'{sat_tag}06_L2', self.filename_tag, day_interval=1, 
+        #                                     server='https://ladsweb.modaps.eosdis.nasa.gov',
+        #                                     fdir_prefix='/archive/allData',fdir_out=modis_fdir, run=run)
+        
+        # MODIS 07 atmospheric profile file
+        self.fnames['mod_07'] = download_laads_https(self.date, f'{sat_tag}07_L2', self.filename_tag, day_interval=1, 
+                                            server='https://ladsweb.modaps.eosdis.nasa.gov',
+                                            fdir_prefix='/archive/allData',fdir_out=modis_fdir, run=run)
+        
+        print("Downloaded MODIS 07 files:", self.fnames['mod_07'])
+        # MOD09A1 surface reflectance product
+        # self.fnames['mod_09'] = []
+        # filename_tags_09 = get_sinusoidal_grid_tag(lon, lat)
+        # for filename_tag in filename_tags_09:
+        #     fnames_09 = download_laads_https(self.date, f'{sat_tag}09A1', filename_tag, day_interval=8, 
+        #                                      server='https://ladsweb.modaps.eosdis.nasa.gov',
+        #                                      fdir_prefix='/archive/allData',fdir_out=modis_fdir, run=run)
+        #     self.fnames['mod_09'] += fnames_09
+
+
+    def dump(self, fname):
+        """
+        Save the SatelliteDownload object into a pickle file
+        """
+        self.fname = fname
+        with open(f'{self.fdir_out}/{fname}', 'wb') as f:
+            if self.verbose:
+                print('Message [satellite_download]: Saving object into %s ...' % fname)
+            pickle.dump(self, f)
 
 
 def prepare_atmospheric_profile(fdir_data, date_s, case_tag, ileg, date, time_start, time_end,
@@ -45,7 +161,6 @@ def prepare_atmospheric_profile(fdir_data, date_s, case_tag, ileg, date, time_st
                                 ):
     
     from er3t.util.modis import get_filename_tag
-    from util.modis07_download import modis_download
     
     
     mod_extent = [np.float64(i) for i in mod_extent]
