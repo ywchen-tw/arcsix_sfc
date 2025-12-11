e3f7a51d15b3ccc726171195ab092bfc5aa700a3
Yu-Wen Chen <r05223131@ntu.edu.tw>
2025-12-11 16:58:29 -0700

    cre update

--- lrt_sim/ssfr_cre.py
+++ lrt_sim/ssfr_cre.py
@@ -1101,29 +1101,6 @@ if __name__ == '__main__':
     #                     manual_alb='sfc_alb_20240603_13.620_13.750_0.32km_cre_alb.dat',
     #                     )
     
-    for lw in [False, True]:
-        cre_sim(date=datetime.datetime(2024, 6, 7),
-                        tmhr_ranges_select=[[15.319, 15.763], # 100m, cloudy
-                                            ],
-                        case_tag='cloudy_atm_corr',
-                        config=config,
-                        levels=np.concatenate((np.array([0.0, 0.1, 0.15, 0.2, 0.43, 0.5, 0.6, 0.8, 1.0,]),
-                                                np.array([1.5, 2.0, 2.5, 3.0, 4.0]), 
-                                                np.arange(5.0, 10.1, 2.5),
-                                                np.array([15, 20, 30., 40., 45.]))),
-                        simulation_interval=0.5,
-                        clear_sky=False,
-                        overwrite_lrt=atm_corr_overwrite_lrt,
-                        manual_cloud=True,
-                        manual_cloud_cer=6.7,
-                        manual_cloud_cwp=26.96/1000,
-                        manual_cloud_cth=0.43,
-                        manual_cloud_cbh=0.15,
-                        manual_cloud_cot=6.02,
-                        lw=lw,
-                        manual_alb='sfc_alb_20240807_13.344_13.761_0.13km_cre_alb.dat',
-                        )
-    
     # for lw in [False, True]:
     #     cre_sim(date=datetime.datetime(2024, 6, 7),
     #                     tmhr_ranges_select=[[15.319, 15.763], # 100m, cloudy
@@ -1144,8 +1121,31 @@ if __name__ == '__main__':
     #                     manual_cloud_cbh=0.15,
     #                     manual_cloud_cot=6.02,
     #                     lw=lw,
-    #                     manual_alb='sfc_alb_20240613_14.109_14.140_0.11km_cre_alb.dat',
+    #                     manual_alb='sfc_alb_20240807_13.344_13.761_0.13km_cre_alb.dat',
     #                     )
+    
+    for lw in [False, True]:
+        cre_sim(date=datetime.datetime(2024, 6, 7),
+                        tmhr_ranges_select=[[15.319, 15.763], # 100m, cloudy
+                                            ],
+                        case_tag='cloudy_atm_corr',
+                        config=config,
+                        levels=np.concatenate((np.array([0.0, 0.1, 0.15, 0.2, 0.43, 0.5, 0.6, 0.8, 1.0,]),
+                                                np.array([1.5, 2.0, 2.5, 3.0, 4.0]), 
+                                                np.arange(5.0, 10.1, 2.5),
+                                                np.array([15, 20, 30., 40., 45.]))),
+                        simulation_interval=0.5,
+                        clear_sky=False,
+                        overwrite_lrt=atm_corr_overwrite_lrt,
+                        manual_cloud=True,
+                        manual_cloud_cer=6.7,
+                        manual_cloud_cwp=26.96/1000,
+                        manual_cloud_cth=0.43,
+                        manual_cloud_cbh=0.15,
+                        manual_cloud_cot=6.02,
+                        lw=lw,
+                        manual_alb='sfc_alb_20240613_14.109_14.140_0.11km_cre_alb.dat',
+                        )
     
     # for lw in [False, True]:
     #     cre_sim(date=datetime.datetime(2024, 6, 7),
