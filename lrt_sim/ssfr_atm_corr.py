e500afaaf793c6f6878a1d766928c3b919268c62
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-07 01:52:44 -0700

    ssfr atm corr update

--- lrt_sim/ssfr_atm_corr.py
+++ lrt_sim/ssfr_atm_corr.py
@@ -2610,15 +2610,43 @@ if __name__ == '__main__':
     #                     )
     
     
+    # for iter in range(3):
+    #     flt_trk_atm_corr(date=datetime.datetime(2024, 7, 29),
+    #                     tmhr_ranges_select=[
+    #                                         [15.214, 15.804], # 1.3km
+    #                                         [16.176, 16.304], # 1.3km
+    #                                         ],
+    #                     case_tag='clear_atm_corr_2',
+    #                     config=config,
+    #                     simulation_interval=0.5,
+    #                     clear_sky=True,
+    #                     overwrite_lrt=atm_corr_overwrite_lrt,
+    #                     manual_cloud=False,
+    #                     manual_cloud_cer=0.0,
+    #                     manual_cloud_cwp=0.0,
+    #                     manual_cloud_cth=0.0,
+    #                     manual_cloud_cbh=0.0,
+    #                     manual_cloud_cot=0.0,
+    #                     iter=iter,
+    #                     )
+        
+    
     for iter in range(3):
-        flt_trk_atm_corr(date=datetime.datetime(2024, 7, 29),
-                        tmhr_ranges_select=[
-                                            [15.214, 15.804], # 1.3km
-                                            [16.176, 16.304], # 1.3km
+        flt_trk_atm_corr(date=datetime.datetime(2024, 7, 30),
+                        tmhr_ranges_select=[[13.886, 13.908],
+                                            [13.934, 13.950],
+                                            [13.976, 14.000],
+                                            [14.031, 14.051],
+                                            [14.073, 14.096],
+                                            [14.115, 14.134],
+                                            [14.157, 14.179],
+                                            [14.202, 14.219],
+                                            [14.239, 14.254],
+                                            [14.275, 14.294],
                                             ],
-                        case_tag='clear_atm_corr_2',
+                        case_tag='clear_sky_spiral_atm_corr',
                         config=config,
-                        simulation_interval=0.5,
+                        simulation_interval=None,
                         clear_sky=True,
                         overwrite_lrt=atm_corr_overwrite_lrt,
                         manual_cloud=False,
@@ -2629,34 +2657,6 @@ if __name__ == '__main__':
                         manual_cloud_cot=0.0,
                         iter=iter,
                         )
-        
-    # done
-    # # for iter in range(3):
-    # #     flt_trk_atm_corr(date=datetime.datetime(2024, 7, 30),
-    # #                     tmhr_ranges_select=[[13.886, 13.908],
-    # #                                         [13.934, 13.950],
-    # #                                         [13.976, 14.000],
-    # #                                         [14.031, 14.051],
-    # #                                         [14.073, 14.096],
-    # #                                         [14.115, 14.134],
-    # #                                         [14.157, 14.179],
-    # #                                         [14.202, 14.219],
-    # #                                         [14.239, 14.254],
-    # #                                         [14.275, 14.294],
-    # #                                         ],
-    # #                     case_tag='clear_sky_spiral_atm_corr',
-    # #                     config=config,
-    # #                     simulation_interval=None,
-    # #                     clear_sky=True,
-    # #                     overwrite_lrt=atm_corr_overwrite_lrt,
-    # #                     manual_cloud=False,
-    # #                     manual_cloud_cer=0.0,
-    # #                     manual_cloud_cwp=0.0,
-    # #                     manual_cloud_cth=0.0,
-    # #                     manual_cloud_cbh=0.0,
-    # #                     manual_cloud_cot=0.0,
-    # #                     iter=iter,
-    # #                     )
     
     # done
     # # for iter in range(3):
