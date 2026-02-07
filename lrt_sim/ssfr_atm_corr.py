4479f415c6939dbee470444d626f706579558c46
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-07 01:51:16 -0700

    ssfr atm corr update

--- lrt_sim/ssfr_atm_corr.py
+++ lrt_sim/ssfr_atm_corr.py
@@ -2541,53 +2541,53 @@ if __name__ == '__main__':
     #                     )
     
 
+    # for iter in range(3):
+    #     flt_trk_atm_corr(date=datetime.datetime(2024, 7, 25),
+    #                     tmhr_ranges_select=[[15.881, 15.903], # 200-500m
+    #                                         ],
+    #                     case_tag='cloudy_atm_corr_2',
+    #                     config=config,
+    #                     levels=np.concatenate((np.array([0.0, 0.1, 0.16, 0.2, 0.3, 0.4, 0.6, 0.8, 1.0,]),
+    #                                            np.array([1.5, 2.0, 2.5, 3.0, 4.0]), 
+    #                                            np.arange(5.0, 10.1, 2.5),
+    #                                            np.array([15, 20, 30., 40., 45.]))),
+    #                     simulation_interval=0.5,
+    #                     clear_sky=False,
+    #                     overwrite_lrt=atm_corr_overwrite_lrt,
+    #                     manual_cloud=True,
+    #                     manual_cloud_cer=11.4,
+    #                     manual_cloud_cwp=9.94/1000,
+    #                     manual_cloud_cth=0.30,
+    #                     manual_cloud_cbh=0.16,
+    #                     manual_cloud_cot=1.31,
+    #                     iter=iter,
+    #                     )
+        
+    
     for iter in range(3):
-        flt_trk_atm_corr(date=datetime.datetime(2024, 7, 25),
-                        tmhr_ranges_select=[[15.881, 15.903], # 200-500m
+        flt_trk_atm_corr(date=datetime.datetime(2024, 7, 29),
+                        tmhr_ranges_select=[[13.442, 13.465],
+                                            [13.490, 13.514],
+                                            [13.536, 13.554],
+                                            [13.580, 13.611],
+                                            [13.639, 13.654],
+                                            [13.676, 13.707],
+                                            [13.733, 13.775],
+                                            [13.793, 13.836],
                                             ],
-                        case_tag='cloudy_atm_corr_2',
+                        case_tag='clear_sky_spiral_atm_corr',
                         config=config,
-                        levels=np.concatenate((np.array([0.0, 0.1, 0.16, 0.2, 0.3, 0.4, 0.6, 0.8, 1.0,]),
-                                               np.array([1.5, 2.0, 2.5, 3.0, 4.0]), 
-                                               np.arange(5.0, 10.1, 2.5),
-                                               np.array([15, 20, 30., 40., 45.]))),
-                        simulation_interval=0.5,
-                        clear_sky=False,
+                        simulation_interval=None,
+                        clear_sky=True,
                         overwrite_lrt=atm_corr_overwrite_lrt,
-                        manual_cloud=True,
-                        manual_cloud_cer=11.4,
-                        manual_cloud_cwp=9.94/1000,
-                        manual_cloud_cth=0.30,
-                        manual_cloud_cbh=0.16,
-                        manual_cloud_cot=1.31,
+                        manual_cloud=False,
+                        manual_cloud_cer=0.0,
+                        manual_cloud_cwp=0.0,
+                        manual_cloud_cth=0.0,
+                        manual_cloud_cbh=0.0,
+                        manual_cloud_cot=0.0,
                         iter=iter,
                         )
-        
-    # done
-    # # for iter in range(3):
-    # #     flt_trk_atm_corr(date=datetime.datetime(2024, 7, 29),
-    # #                     tmhr_ranges_select=[[13.442, 13.465],
-    # #                                         [13.490, 13.514],
-    # #                                         [13.536, 13.554],
-    # #                                         [13.580, 13.611],
-    # #                                         [13.639, 13.654],
-    # #                                         [13.676, 13.707],
-    # #                                         [13.733, 13.775],
-    # #                                         [13.793, 13.836],
-    # #                                         ],
-    # #                     case_tag='clear_sky_spiral_atm_corr',
-    # #                     config=config,
-    # #                     simulation_interval=None,
-    # #                     clear_sky=True,
-    # #                     overwrite_lrt=atm_corr_overwrite_lrt,
-    # #                     manual_cloud=False,
-    # #                     manual_cloud_cer=0.0,
-    # #                     manual_cloud_cwp=0.0,
-    # #                     manual_cloud_cth=0.0,
-    # #                     manual_cloud_cbh=0.0,
-    # #                     manual_cloud_cot=0.0,
-    # #                     iter=iter,
-    # #                     )
         
     # done
     # # for iter in range(3):
