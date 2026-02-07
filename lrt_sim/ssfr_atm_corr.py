0368030040587e327aec480d7edf794aec06fa51
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-02-07 01:57:56 -0700

    ssfr atm corr update

--- lrt_sim/ssfr_atm_corr.py
+++ lrt_sim/ssfr_atm_corr.py
@@ -2683,31 +2683,11 @@ if __name__ == '__main__':
     
     
     
-    for iter in range(3):
-        flt_trk_atm_corr(date=datetime.datetime(2024, 8, 1),
-                        tmhr_ranges_select=[[13.843, 14.361], # 100-450m, clear, some open ocean
-                                            ],
-                        case_tag='clear_atm_corr',
-                        config=config,
-                        simulation_interval=0.5,
-                        clear_sky=True,
-                        overwrite_lrt=atm_corr_overwrite_lrt,
-                        manual_cloud=False,
-                        manual_cloud_cer=0.0,
-                        manual_cloud_cwp=0.0,
-                        manual_cloud_cth=0.0,
-                        manual_cloud_cbh=0.0,
-                        manual_cloud_cot=0.0,
-                        iter=iter,
-                        )
-    
-    # done
     # for iter in range(3):
     #     flt_trk_atm_corr(date=datetime.datetime(2024, 8, 1),
-    #                     tmhr_ranges_select=[
-    #                                         [14.739, 15.053], # 550m
+    #                     tmhr_ranges_select=[[13.843, 14.361], # 100-450m, clear, some open ocean
     #                                         ],
-    #                     case_tag='clear_atm_corr_2',
+    #                     case_tag='clear_atm_corr',
     #                     config=config,
     #                     simulation_interval=0.5,
     #                     clear_sky=True,
@@ -2720,6 +2700,26 @@ if __name__ == '__main__':
     #                     manual_cloud_cot=0.0,
     #                     iter=iter,
     #                     )
+    
+    
+    for iter in range(3):
+        flt_trk_atm_corr(date=datetime.datetime(2024, 8, 1),
+                        tmhr_ranges_select=[
+                                            [14.739, 15.053], # 550m
+                                            ],
+                        case_tag='clear_atm_corr_2',
+                        config=config,
+                        simulation_interval=0.5,
+                        clear_sky=True,
+                        overwrite_lrt=atm_corr_overwrite_lrt,
+                        manual_cloud=False,
+                        manual_cloud_cer=0.0,
+                        manual_cloud_cwp=0.0,
+                        manual_cloud_cth=0.0,
+                        manual_cloud_cbh=0.0,
+                        manual_cloud_cot=0.0,
+                        iter=iter,
+                        )
         
    
     # done
