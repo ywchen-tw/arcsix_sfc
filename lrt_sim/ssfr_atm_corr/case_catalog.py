2f989041dbdaa3187141c25ea8de10299b021768
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-09-13 17:52:52 -0600

    case_id update

--- lrt_sim/ssfr_atm_corr/case_catalog.py
+++ lrt_sim/ssfr_atm_corr/case_catalog.py
@@ -437,7 +437,7 @@ ALL_CASE_CATALOG = [
   'date': '2024-06-07',
   'case_tag': 'cloudy_atm_corr',
   'tmhr_ranges_select': [
-                        [15.319, 15.5025], # 100m, cloudy
+                        [15.319, 15.411], # 100m, cloudy
                         ],
   'simulation_interval': 0.5,
   'clear_sky': False,
@@ -453,11 +453,11 @@ ALL_CASE_CATALOG = [
   'manual_cloud_cbh': 0.15,
   'manual_cloud_cot': 6.02,},
  
- {'id': 'case_054',
+ {'id': 'case_059',
    'date': '2024-06-07',
    'case_tag': 'cloudy_atm_corr',
    'tmhr_ranges_select': [
-                         [15.5025, 15.669], # 100m, cloudy
+                         [15.411, 15.5025], # 100m, cloudy
                          ],
    'simulation_interval': 0.5,
    'clear_sky': False,
@@ -472,6 +472,46 @@ ALL_CASE_CATALOG = [
    'manual_cloud_cth': 0.43,
    'manual_cloud_cbh': 0.15,
    'manual_cloud_cot': 6.02,},
+ 
+ {'id': 'case_054',
+   'date': '2024-06-07',
+   'case_tag': 'cloudy_atm_corr',
+   'tmhr_ranges_select': [
+                         [15.5025, 15.586], # 100m, cloudy
+                         ],
+   'simulation_interval': 0.5,
+   'clear_sky': False,
+   'manual_cloud': True,
+   'levels': np.concatenate((np.array([0.0, 0.1, 0.15, 0.2, 0.43, 0.5, 0.6, 0.8, 1.0,]),
+                             np.array([1.5, 2.0, 2.5, 3.0, 4.0]), 
+                             np.arange(5.0, 10.1, 2.5),
+                             np.array([15, 20, 30., 40., 45.]))),
+   'manual_cloud_cer': 6.7,
+   'manual_cloud_cwp': 0.02696,
+   'manual_cloud_cwp_expr': '26.96 / 1000',
+   'manual_cloud_cth': 0.43,
+   'manual_cloud_cbh': 0.15,
+   'manual_cloud_cot': 6.02,},
+ 
+ {'id': 'case_060',
+    'date': '2024-06-07',
+    'case_tag': 'cloudy_atm_corr',
+    'tmhr_ranges_select': [
+                          [15.586, 15.669], # 100m, cloudy
+                          ],
+    'simulation_interval': 0.5,
+    'clear_sky': False,
+    'manual_cloud': True,
+    'levels': np.concatenate((np.array([0.0, 0.1, 0.15, 0.2, 0.43, 0.5, 0.6, 0.8, 1.0,]),
+                              np.array([1.5, 2.0, 2.5, 3.0, 4.0]), 
+                              np.arange(5.0, 10.1, 2.5),
+                              np.array([15, 20, 30., 40., 45.]))),
+    'manual_cloud_cer': 6.7,
+    'manual_cloud_cwp': 0.02696,
+    'manual_cloud_cwp_expr': '26.96 / 1000',
+    'manual_cloud_cth': 0.43,
+    'manual_cloud_cbh': 0.15,
+    'manual_cloud_cot': 6.02,},
  
  {'id': 'case_055',
    'date': '2024-06-07',
