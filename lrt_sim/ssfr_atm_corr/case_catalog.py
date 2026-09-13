f1c6cbecf320916b220a32ee9479cfdd8b684025
Yu-Wen Chen <r05223131@ntu.edu.tw>
2026-09-13 17:28:35 -0600

    R1 and case_id update

--- lrt_sim/ssfr_atm_corr/case_catalog.py
+++ lrt_sim/ssfr_atm_corr/case_catalog.py
@@ -417,7 +417,7 @@ ALL_CASE_CATALOG = [
   'date': '2024-06-07',
   'case_tag': 'cloudy_atm_corr',
   'tmhr_ranges_select': [
-                        [15.319, 15.502], # 100m, cloudy
+                        [15.319, 15.5025], # 100m, cloudy
                         ],
   'simulation_interval': 0.5,
   'clear_sky': False,
@@ -437,7 +437,7 @@ ALL_CASE_CATALOG = [
    'date': '2024-06-07',
    'case_tag': 'cloudy_atm_corr',
    'tmhr_ranges_select': [
-                         [15.502, 15.669], # 100m, cloudy
+                         [15.5025, 15.669], # 100m, cloudy
                          ],
    'simulation_interval': 0.5,
    'clear_sky': False,
